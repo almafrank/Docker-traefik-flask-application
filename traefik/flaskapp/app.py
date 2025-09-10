dacfed9b5a43155ad0a5387505ea367a8617f7d8
almafrank <alma.k.frank@hotmail.com>
2025-09-11 00:34:13 +0200

    Added a few changes to the compose yaml file

--- traefik/flaskapp/app.py
+++ traefik/flaskapp/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from redis import Redis
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,4 @@ def hello():
 	return "Hello World!. I am back"
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=8000, debug=True)
